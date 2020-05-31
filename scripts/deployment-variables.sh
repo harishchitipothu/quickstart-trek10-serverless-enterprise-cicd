e3558acfd3eec0126c3b9301bf612c17e6dbef71
harishchitipothu <57835771+harishchitipothu@users.noreply.github.com>
2020-06-01 00:01:25 +0800

    updated

--- scripts/deployment-variables.sh
+++ scripts/deployment-variables.sh
@@ -20,11 +20,11 @@ LOCAL_REPO_FOLDER=../sample-project-codecommit
 #####################
 # Name of an S3 bucket to be created -- MUST BE GLOBALLY UNIQUE!
 # This must be set to a new value
-S3_BUCKET=MY-UNIQUE-BUCKETNAME
+S3_BUCKET=serverless-sample-s3-bucket
 # AWS Account IDs
 # These must be set to the correct values
-DEV_ACCOUNT_ID=123456789123
-PROD_ACCOUNT_ID=123456789123
+DEV_ACCOUNT_ID=174127492659
+PROD_ACCOUNT_ID=465853763517
 # Prefix for files in S3 bucket. 
 # Default is fine for most scenarios, this can optionally be adjusted
-S3_PREFIX=AWS-CICD-Quickstart
+S3_PREFIX=aws-cicd-quickstart
